e7bec67bbe5bcfd3af7a79d4f7cfc7aab958a3b6
Davi Jose Araujo Filho <davijose@gmail.com>
2023-10-01 21:28:10 -0300

    feat: added AsyncStorage

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import uuid from 'react-native-uuid';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { styles } from './styles';
 
 import { Input } from '../../components/Input';
@@ -10,8 +10,27 @@ import { Button } from '../../components/Button';
 import { HeaderForm } from '../../components/HeaderForm';
 
 export function Form() {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
-
+  async function handleNew() {
+    try {
+      const id = uuid.v4();
+      const newData = {
+        id,
+        name,
+        user,
+        password
+      }
+      await AsyncStorage.setItem("@savePass:passwords", JSON.stringify(newData));
+      console.log(newData);
+      Toast.show({type: 'success', text1: 'Cadastrado com sucesso'})
+    } catch(error) {
+      console.log(error);
+      Toast.show({type: 'error', text1: 'Nao foi possivel cadastrar.'})
+    } 
+  }
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -25,20 +44,27 @@ export function Form() {
           <View style={styles.form}>
             <Input
               label="Nome do serviço"
+              onChangeText={setName}
+              value={name}
             />
             <Input
               label="E-mail ou usuário"
               autoCapitalize="none"
+              onChangeText={setUser}
+              value={user}
             />
             <Input
               label="Senha"
               secureTextEntry
+              onChangeText={setPassword}
+              value={password}
             />
           </View>
 
           <View style={styles.footer}>
             <Button
               title="Salvar"
+              onPress={handleNew}
             />
           </View>
         </ScrollView>
